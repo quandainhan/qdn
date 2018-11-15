739eca1d68f7aaf243a057fb8ad6f2a78d2bb65a
QQuân Đại Nhân <xuanquan622@gmail.com>
2018-11-15 22:35:32 +0700

    Add files via upload

---
+++ newprogram.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int  main(){
+	
+	char sinhVien[10];
+	int score;
+	printf("nhap ten sinh vien: ");
+	gets(sinhVien);
+	if(score == 6996){
+	printf("next level 2 di de~ et!!");
+	}
+	else
+	printf("core = %d ",score);
+
+
+
+return 0;
+}
